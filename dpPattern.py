3f20b97075ae2f2679eb52a7d8e36d19adc51d5b
Will Luong <tluong@stedwards.edu>
2020-10-06 16:30:12 -0500

    Update dpPattern.py
    update the file with dp implmentation of the coin change problem

--- dpPattern.py
+++ dpPattern.py
@@ -82,6 +82,64 @@ def minPathSum(grid):
 
     return grid[-1][-1]
 
+#Leetcode 322: Coin change
+
+#Problem statement: 
+'''
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+Example 4:
+
+Input: coins = [1], amount = 1
+Output: 1
+Example 5:
+
+Input: coins = [1], amount = 2
+Output: 2
+ 
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+'''
+def coinChange(coins, amount):
+    #base case: 
+    if not coins or not amount: 
+        return 0
+    #if they are both empty
+    if not coins and not amount: 
+        return 0
+    #create a dp array to run the dp algorithm from the bottom up
+    dp = [float("inf")] * (amount + 1)
+    dp[0] = 0
+
+    for coin in coins: 
+        for j in range(1, amount + 1):
+            dp[j] = min(dp[j], dp[j - coin] + 1)
+
+    return dp[amount] if dp[amount] != float("inf") else -1 
+
+
+
+
 #main function to excute different implementation
 def main():
     print("TESTING MIN COST CLIMBING STAIRS...")
@@ -103,5 +161,13 @@ def main():
     print("END OF TESTING...")
 
     print("")
+
+    print("TESTING COIN CHANGE...")
+    test_coin = [1,2,5]
+    test_amount = 11
+    print(coinChange(test_coin, test_amount))
+    # print(coinChange(test_coin, test_amount))
+    # print(coinChange(test_coin, test_amount))
+    print("END OF TESTING...") 
 
 main()
