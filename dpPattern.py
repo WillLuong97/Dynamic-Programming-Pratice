7e53d53b9142afe32475f662d69df164377a6884
Will Luong <tluong@stedwards.edu>
2020-10-06 14:37:51 -0500

    Update dpPattern.py
    Update the file with the source code for leetcode # 736: Min cost climbing stairs

--- dpPattern.py
+++ dpPattern.py
@@ -18,10 +18,31 @@ Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3]
 Note:
 cost will have a length in the range [2, 1000].
 Every cost[i] will be an integer in the range [0, 999].
-
+#Time complexity: 
+O(n) where n is the length of the cost
+O(1) the space complexity is used by 1 and 2 
 '''
+def minCostClimbingStairs(cost):
+    #base case: 
+    if not cost:
+        return None
+
+    #two poiter: 
+    p1 = 0
+    p2 = 0
+    #loop through each cost
+    for i in reversed(cost): 
+        #swap the element: 
+        p1, p2 = i + min(p1, p2), p1
+        
+    return min(p1, p2)
 
 #main function to excute different implementation
 def main():
-    pass
+    print("TESTING MIN COST CLIMBING STAIRS...")
+    testCost = [10, 15, 20]
+    testCost01 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+    print(minCostClimbingStairs(testCost))
+    print(minCostClimbingStairs(testCost01))
+
 main()
